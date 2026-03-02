df23fbc0922c16418dce72abf34cac5cdbff13c1
Axel Bigot <abigot@uci.edu>
2026-03-01 16:44:18 -0800

    chore: baseline mlp

--- src/variants/__init__.py
+++ src/variants/__init__.py
@@ -1,9 +1,11 @@
 from src.variants.example import *
 from src.variants.mel_cnn import *
 from src.variants.mfcc_cnn import *
+from src.variants.baseline import *
 
 __all__ = [
 	'ExampleFMAModel',
 	'MelCNNFMAModel',
     'MFCC_CNNFMAModel',
+	'MelMLPFMAModel',
 ]
